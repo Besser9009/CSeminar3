263fa78ca5a7b0364f842c807b15c2904e38a8c4
besser <besser9009@mail.ru>
2023-04-02 15:08:01 +0300

    add Number3

---
+++ Number3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+System.Console.Write("Введите сумму вклада: ");
+double Vklad = Convert.ToDouble(Console.ReadLine());
+double OneProcent = Vklad / 100;
+if (Vklad < 100 && Vklad > 0){
+    double Procent = 5 * OneProcent;
+    System.Console.WriteLine($"Общая сумма с %: {Vklad + Procent}");
+}
+else if (Vklad > 100 && Vklad < 200){
+    double Procent = 7 * OneProcent;
+    System.Console.WriteLine($"Общая сумма с %: {Vklad + Procent}");
+}
+else if (Vklad > 200){
+    double Procent = 10 * OneProcent;
+    System.Console.WriteLine($"Общая сумма с %: {Vklad + Procent}");
+}
+else{
+    System.Console.WriteLine("Вклада нет ):");
+}
