b7da460edd34dbace683fe59cb7c8769505b96fd
besser <besser9009@mail.ru>
2023-04-02 14:35:50 +0300

    add Number2

---
+++ Number2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+System.Console.Write("Введите номер искомой четверти: ");
+int Section = int.Parse(Console.ReadLine());
+if (Section == 1){
+    System.Console.WriteLine("Диапозон x > 0; y > 0");
+}
+else if ( Section == 2){
+    System.Console.WriteLine("Диапозон x < 0; y > 0");
+}
+else if ( Section == 3){
+    System.Console.WriteLine("Диапозон x < 0; y < 0");
+}
+else if ( Section == 4){
+    System.Console.WriteLine("Диапозон x > 0; y < 0");
+}
+else{
+    System.Console.WriteLine("Неверно введён номер четверти");
+}
