f0e2f46df55157f9b8df58bb9d21e79613adb8e7
besser <besser9009@mail.ru>
2023-04-04 18:50:52 +0300

    add new decision in file Number5

--- Number5/Program.cs
+++ Number5/Program.cs
@@ -1,8 +1,16 @@
 ﻿Console.Clear();
+//Первое решение
 System.Console.Write("Введите число N: ");
 int NumN = int.Parse(Console.ReadLine());
 int NumX = 1;
 while(NumX <= NumN){
     System.Console.Write($"{Math.Pow(NumX, 2)} ");
     NumX++;
+}
+System.Console.WriteLine(" ");
+//Второе решение
+System.Console.Write("Введите число N: ");
+int NumZ = int.Parse(Console.ReadLine());
+for (int NumR = 1; NumR <= NumZ; NumR++){
+    Console.Write($"{Math.Pow(NumR, 2)} ");
 }
