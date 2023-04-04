ea3e4e168ce40399834be31207166c88760d1aaf
besser <besser9009@mail.ru>
2023-04-04 18:40:06 +0300

    add Number5

---
+++ Number5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+System.Console.Write("Введите число N: ");
+int NumN = int.Parse(Console.ReadLine());
+int NumX = 1;
+while(NumX <= NumN){
+    System.Console.WriteLine($"{Math.Pow(NumX, 2)}");
+    NumX++;
+}
